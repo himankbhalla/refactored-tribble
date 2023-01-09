072b6b69a946c7a7b75312b783ed08a97d9e8f86
himankbhalla <himankbhalla@himanks-MacBook-Air.local>
2023-01-09 10:59:38 +0530

    module included, fixed tests

--- dpk.js
+++ dpk.js
@@ -11,6 +11,9 @@ exports.deterministicPartitionKey = (event, TRIVIAL_PARTITION_KEY = "0", MAX_PAR
   if (!event) {
     return TRIVIAL_PARTITION_KEY;
   }
-  candidate = event.partitionKey ? JSON.stringify(event.partitionKey) : crypto.createHash(hashOps.algo).update(JSON.stringify(event)).digest(hashOps.digestEncoding);
+  if (event.partionKey && typeof event.partitionKey !== "string") {
+    event.partitionKey = JSON.stringify(event.partitionKey);
+  }
+  candidate = event.partitionKey ? event.partitionKey : crypto.createHash(hashOps.algo).update(JSON.stringify(event)).digest(hashOps.digestEncoding);
   return candidate.length > MAX_PARTITION_KEY_LENGTH ? crypto.createHash(hashOps.algo).update(candidate).digest(hashOps.digestEncoding) : candidate;
 };
